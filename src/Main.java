9a9ad6188826101aa7d1cca24b4d25347016b108
AlessioStrafo <straforini.alessio@gmail.com>
2024-02-07 12:46:57 +0100

    Esercizio rivalutato

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,32 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Student studente1 = new Student("Mario", 20);
-        Student studente2 = new Student("Romina", 22);
-        Student studente3 = new Student("Pietro", 24);
-        Student studente4 = new Student("Maria", 25);
+//        Student studente1 = new Student("Mario", 20);
+//        Student studente2 = new Student("Romina", 22);
+//        Student studente3 = new Student("Pietro", 24);
+//        Student studente4 = new Student("Maria", 25);
+//
+//        List<Student> studente = new ArrayList<>();
+//        studente.add(studente1);
+//        studente.add(studente2);
+//        studente.add(studente3);
+//        studente.add(studente4);
+//        System.out.println(studente);
 
-        List<Student> studente = new ArrayList<>();
-        studente.add(studente1);
-        studente.add(studente2);
-        studente.add(studente3);
-        studente.add(studente4);
-        System.out.println(studente);
+        ArrayList<Student> studenti = new ArrayList<>();
+        studenti.add(new Student("Raul", 43));
+        studenti.add(new Student("Gioia",20));
+        for (Student student:studenti) {
+            System.out.println(student);
+        }
+        System.out.println("-------------------------");
+        studenti.add(new Student("Gabri",22));
+        studenti.add(new Student("Mario",24));
+        studenti.add(new Student("Maria",28));
+        studenti.add(new Student("Teresa",21));
+
+        for (Student student:studenti){
+            System.out.println(student);
+        }
     }
 }
